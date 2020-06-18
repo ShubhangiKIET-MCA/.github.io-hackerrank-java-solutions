451ccf31f31b8d6af2c880533dfcb3c9a7e86dc0
Shubhangi <65587172+ShubhangiKIET-MCA@users.noreply.github.com>
2020-06-17 23:10:40 -0700

    Add files via upload

---
+++ java loop1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+
+            System.out.printf("%d x %d = %d%n", N, i, N*i);
+        }
+    }
+}
